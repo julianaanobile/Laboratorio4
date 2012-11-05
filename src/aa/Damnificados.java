7c455f15cb1423be9a4ccf08ab0bb44edf2964f5
Andrea Quiceno <pixel.617@gmail.com>
2012-11-04 20:12:34 -0500

    update hasta menor damnificados

--- src/aa/Damnificados.java
+++ src/aa/Damnificados.java
@@ -7,6 +7,10 @@ public class Damnificados {
     String [] departamentos = {"Bolivar","Magdalena","Atlantico","Guajira","Cordoba","Sucre"};//Filas en la matriz
     String [] anios = {"2005","2006","2007","2008"};//columnas en la matriz
     
+    int [] damnificadosPorAnio = new int[anios.length];
+    int menorNumeroDamnificados;
+    int indiceDept;
+    
     int[][] damnificados = {
         {110698, 125147, 257321, 389010},
         {98742, 123654, 325698, 282965},
@@ -17,9 +21,22 @@ public class Damnificados {
     
     public Damnificados() {
         
+        calcularDamnificadosPorAnio();
+        calcularDptoMenorNumeroDamnificado();
+    }
+
+    private void calcularDamnificadosPorAnio() {
+        int sumaAux = 0;
+        
+        for (int i = 0; i < damnificados.length; i++) {
+            for (int j = 0; j < damnificados[i].length; j++) {
+                sumaAux = damnificados[i][j];
+            }
+            damnificadosPorAnio[i]=sumaAux/damnificados[0].length;
+        }
+    }
+
+    private void calcularDptoMenorNumeroDamnificado() {
         
     }
-    
-    
-    
 }
